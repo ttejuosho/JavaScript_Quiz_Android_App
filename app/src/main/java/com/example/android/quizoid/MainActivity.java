57e6e9903e707199a239a9ecaefb9b2d4f4c2763
Taiwo <theycallmeflowz@yahoo.com>
2018-04-03 19:18:56 -0500

    QXML

--- app/src/main/java/com/example/android/quizoid/MainActivity.java
+++ app/src/main/java/com/example/android/quizoid/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.quizoid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void answerCheck(View view){
+        boolean checked =( (RadioButton) view ).isChecked();
+
+        switch (view.getId()){
+            case R.id.q1Answer:
+                if (checked) {
+                    score = score + 1;
+                } else {
+                    score = score;
+                }
+
+            case R.id.q2Answer:
+                if (checked) {
+                    score = score + 1;
+                } else {
+                    score = score;
+                }
+
+            case R.id.q3Answer:
+                if (checked) {
+                    score = score + 1;
+                } else {
+                    score = score;
+                }
+
+            case R.id.q4Answer:
+                if (checked) {
+                    score = score + 1;
+                } else {
+                    score = score;
+                }
+
+            case R.id.q5Answer:
+                if (checked) {
+                    score = score + 1;
+                } else {
+                    score = score;
+                    break;
+                }
+        }
     }
 }
